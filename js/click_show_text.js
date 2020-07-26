a3786bcb298c74c3bf3ecd93fbfb798f50f9c5c3
YuJieJun <2642391070@qq.com>
2020-07-26 16:13:54 +0800

    Site updated: 2020-07-26 16:13:54

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -1,7 +1,7 @@
 var a_idx = 0;
 jQuery(document).ready(function ($) {
     $("body").click(function (e) {
-        var a = new Array("我点", "我点", "我再点", "我", "点点", "点点", "点点", "还点吗？", "还点啊!", "算你厉害", "。。。。", "服了");
+        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
